6d108e28e9c9ced5db9f030121345c10346d2d3b
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-10 16:17:38 -0400

    changes to try and fix creation with join table between user and recipe

--- server/controllers/api/recipesController.js
+++ server/controllers/api/recipesController.js
@@ -6,23 +6,25 @@ const jwt = require("jsonwebtoken");
 
 // Route to retrieve all recipes in db
 recipesController.get("/all", (req, res) => {
-    db.Recipe.findAll({}, {include: [{model: db.Category, as: "categories"}, {model: db.Ingredient, as: "ingredients"}]})
-    .then(recipe => res.json(recipe))
-    .catch(err => res.json(err));
+    db.Recipe.findAll({}, { include: [{ model: db.Category, as: "categories" }, { model: db.Ingredient, as: "ingredients" }] })
+        .then(recipe => res.json(recipe))
+        .catch(err => res.json(err));
 });
 
 // Route to create a new recipe
 recipesController.post("/", JWTVerifier, (req, res) => {
     const { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients } = req.body;
+    const users = req.user.id;
 
     // Create the entry in Recipe table
     db.Recipe.create(
-        { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients, createdBy: req.user.username }, 
-        { include: [{model: db.Category, as: "categories"}, {model: db.Ingredient, as: "ingredients"}] }
+        { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients, createdBy: req.user.username, users },
+        { include: [{ model: db.Category, as: "categories" }, { model: db.Ingredient, as: "ingredients" }, { model: db.User, as: "users" }] }
     )
-        .then(recipe => res.json(recipe))
-        .catch(err => res.json(err));
 
+    req.user.addRecipe(
+        
+    )
 });
 
 module.exports = recipesController;
