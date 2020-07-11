73f286b8836013ee9f5b729d918eee793465c770
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-07-10 22:53:45 -0400

    partial commit of validation of categories to prep for recipe association

--- server/controllers/api/recipesController.js
+++ server/controllers/api/recipesController.js
@@ -12,19 +12,39 @@ recipesController.get("/all", (req, res) => {
 });
 
 // Route to create a new recipe
-recipesController.post("/", JWTVerifier, (req, res) => {
+recipesController.post("/", JWTVerifier, async (req, res) => {
     const { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients } = req.body;
-    const users = req.user.id;
 
-    // Create the entry in Recipe table
-    db.Recipe.create(
-        { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients, createdBy: req.user.username, users },
-        { include: [{ model: db.Category, as: "categories" }, { model: db.Ingredient, as: "ingredients" }, { model: db.User, as: "users" }] }
-    )
+    // Validate categories
+    const dbCategoryArray = await db.Category.findAll({})
+    const catList = dbCategoryArray.map(({ dataValues }) => { return { id: dataValues.id, category: dataValues.category } })
+    const existing = [];
+    categories.forEach(({ category }) => {
+        const catIndex = catList.findIndex(c => c.category === category)
+        if (catIndex >= 0) {
+            existing.push(catList[catIndex].id);
+        } else {
+            //create new category and push to existing
+            db.Category.create({ category })
+                .then(existing.push(category))
+        }
+    })
 
-    req.user.addRecipe(
-        
-    )
+
+
+    const dbIngredientArray = await db.Ingredient.findAll({})
+
+    // Create the entry in Recipe table    
+    // await db.Recipe.create(
+    //     { title, image, description, prepTime, cookTime, servings, directions, categories, ingredients, createdBy: req.user.username },
+    //     { include: [{ model: db.Category, as: "categories" }, { model: db.Ingredient, as: "ingredients" }] }
+    // )
+    // .then(
+    //     db.User.setRecipes([recipe.id])
+    // )
+    // .then(recipe => res.json(recipe))
+    // .catch(err => res.json(err));
+
 });
 
 module.exports = recipesController;
